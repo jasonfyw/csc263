b093523c2fec5dcbc0ac530f06c48030ea7c3a37
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2023-02-02 23:05:09 -0500

    add: more max heap operations

--- csc263/heaps/max_heap.py
+++ csc263/heaps/max_heap.py
@@ -1,7 +1,9 @@
-from typing import List
+from typing import List, TypeVar
 from . import heap
 
-def max_heapify(A: List[int], i: int) -> None:
+T = TypeVar('T')
+
+def max_heapify(A: List[T], i: int) -> None:
     l = heap.left(i)
     r = heap.right(i)
     if l <= len(A) - 1 and A[l] > A[i]:
@@ -15,3 +17,35 @@ def max_heapify(A: List[int], i: int) -> None:
     if largest != i:
         A[i], A[largest] = A[largest], A[i]
         max_heapify(A, largest)
+
+
+def build_max_heap(A: List[T]) -> None:
+    for i in range((len(A) - 1) // 2, 0, -1):
+        max_heapify(A, i)
+
+
+def heap_max(A: List[T]) -> T:
+    return A[1]
+
+
+def heap_extract_max(A: List[T]) -> T:
+    if len(A) <= 1:
+        raise ValueError('Heap is empty')
+    max_ = A[1]
+    A[1] = A.pop()
+    max_heapify(A, 1)
+    return max_
+
+
+def heap_increase_key(A: List[T], i: int, key: T) -> None:
+    if key < A[i]:
+        raise ValueError('New key is smaller than current key')
+    A[i] = key
+    while i > 1 and A[heap.parent(i)] < A[i]:
+        A[i], A[heap.parent(i)] = A[heap.parent(i)], A[i]
+        i = heap.parent(i)
+
+
+def max_heap_insert(A: List[T], key: T) -> None:
+    A.append(float('-inf'))
+    heap_increase_key(A, len(A) - 1, key)
